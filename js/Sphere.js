72150641a8b2e325e153bd49e21b0e8a6b95459b
jbphet <john.blanco@colorado.edu>
2019-03-06 13:57:31 -0700

    converted magnitudeSquared to standard getter pattern, see https://github.com/phetsims/dot/issues/87

--- js/Sphere.js
+++ js/Sphere.js
@@ -155,7 +155,7 @@ define( function( require ) {
 
       // basically, we can use the quadratic equation to solve for both possible hit points (both +- roots are the hit points)
       var tmp = rayDir.dot( centerToRay );
-      var centerToRayDistSq = centerToRay.magnitudeSquared();
+      var centerToRayDistSq = centerToRay.magnitudeSquared;
       var det = 4 * tmp * tmp - 4 * ( centerToRayDistSq - this.radius * this.radius );
       if ( det < epsilon ) {
         // ray misses sphere entirely
