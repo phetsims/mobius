cb41c77ed2d3333145e01a5d6aac1b5e2568ad1b
phet-dev <phetadmin@gmail.com>
2021-09-28 03:18:10 -0600

    update copyright dates from daily grunt work

--- js/TextureQuad.js
+++ js/TextureQuad.js
@@ -1,4 +1,4 @@
-// Copyright 2019-2020, University of Colorado Boulder
+// Copyright 2019-2021, University of Colorado Boulder
 
 /**
  * Creates a quad for a given texture with a specific width and height, in the 3d space
