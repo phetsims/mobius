b15c0858eeb0251ca68f492cfa8ea97cab3d35e6
Sam Reid <reids@colorado.edu>
2021-11-18 17:44:13 -0700

    Run grunt update, see https://github.com/phetsims/chipper/issues/1148

--- js/mobiusStrings.ts
+++ js/mobiusStrings.ts
@@ -3,11 +3,17 @@
 /**
  * Auto-generated from modulify, DO NOT manually modify.
  */
-
+/* eslint-disable */
 import getStringModule from '../../chipper/js/getStringModule.js';
 import mobius from './mobius.js';
 
-const mobiusStrings = getStringModule( 'MOBIUS' );
+type StringsType = {
+  'mobius': {
+    'title': string
+  }
+};
+
+const mobiusStrings = getStringModule( 'MOBIUS' ) as StringsType;
 
 mobius.register( 'mobiusStrings', mobiusStrings );
 
