d8749a3214a61d6d024bf75925b92b81b772e4fd
phet-dev <phetadmin@gmail.com>
2021-12-22 03:20:13 -0700

    update copyright dates from daily grunt work

--- js/Quad.js
+++ js/Quad.js
@@ -1,4 +1,4 @@
-// Copyright 2019-2020, University of Colorado Boulder
+// Copyright 2019-2021, University of Colorado Boulder
 
 /**
  * Creates a position/normal/uv mapped quad based on vertices in a high-performance way.
