2c2e4c23a66c2c1cf126183607739c3fb582f781
Sam Reid <reids@colorado.edu>
2022-09-26 12:16:54 -0600

    Change TReadOnlyProperty => LinkableProperty, see https://github.com/phetsims/axon/issues/414

--- js/MobiusStrings.ts
+++ js/MobiusStrings.ts
@@ -5,13 +5,13 @@
  */
 /* eslint-disable */
 import getStringModule from '../../chipper/js/getStringModule.js';
-import TReadOnlyProperty from '../../axon/js/TReadOnlyProperty.js';
+import LinkableProperty from '../../axon/js/LinkableProperty.js';
 import mobius from './mobius.js';
 
 type StringsType = {
   'mobius': {
     'title': string;
-    'titleStringProperty': TReadOnlyProperty<string>;
+    'titleStringProperty': LinkableProperty<string>;
   }
 };
 
