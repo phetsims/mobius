74536ba33c8905c3f80680aea3c8c6b1e4d17f17
Jonathan Olson <jonathan.olson@colorado.edu>
2022-10-27 10:45:10 -0600

    Updating phet-lib with main common code references.

---
+++ js/main.ts
@@ -0,0 +1,17 @@
+// Copyright 2022, University of Colorado Boulder
+
+import mobius from './mobius.js';
+import './MobiusQueryParameters.js';
+import './NodeTexture.js';
+import './Quad.js';
+import './TextureQuad.js';
+import './ThreeInstrumentable.js';
+import './ThreeIsometricNode.js';
+import './ThreeNode.js';
+import './ThreeObject3DPhetioObject.js';
+import './ThreeQuaternionIO.js';
+import './ThreeStage.js';
+import './ThreeUtils.js';
+import './TriangleArrayWriter.js';
+
+export default mobius;
