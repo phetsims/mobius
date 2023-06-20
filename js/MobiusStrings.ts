3bc52e2e765a34b1a8e51c0aec33f6329334ced6
Michael Kauzmann <michael.kauzmann@colorado.edu>
2023-06-20 12:17:03 -0600

    remove as many LinkableElement usages as possible, using PhetioProperty where we have to, https://github.com/phetsims/tandem/issues/299
    Signed-off-by: Michael Kauzmann <michael.kauzmann@colorado.edu>

--- js/MobiusStrings.ts
+++ js/MobiusStrings.ts
@@ -5,12 +5,12 @@
  */
 /* eslint-disable */
 import getStringModule from '../../chipper/js/getStringModule.js';
-import LinkableProperty from '../../axon/js/LinkableProperty.js';
+import type LocalizedStringProperty from '../../chipper/js/LocalizedStringProperty.js';
 import mobius from './mobius.js';
 
 type StringsType = {
   'mobius': {
-    'titleStringProperty': LinkableProperty<string>;
+    'titleStringProperty': LocalizedStringProperty;
   }
 };
 
