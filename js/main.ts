eea2b1e4da73e9cb8159e540a546d28ba3769ec2
pixelzoom <cmalley@pixelzoom.com>
2024-01-05 09:31:07 -0700

    fixes for author-annotation lint rule, https://github.com/phetsims/chipper/issues/1414

--- js/main.ts
+++ js/main.ts
@@ -1,5 +1,9 @@
 // Copyright 2022, University of Colorado Boulder
 
+/**
+ * @author Jonathan Olson <jonathan.olson@colorado.edu>
+ */
+
 import mobius from './mobius.js';
 import './MobiusQueryParameters.js';
 import './NodeTexture.js';
