5056ab45469c5821e661f6b230f4036591b8b662
phet-dev <phetadmin@gmail.com>
2025-01-16 03:29:16 -0700

    update copyright dates from daily grunt work

--- js/MobiusScreenView.ts
+++ js/MobiusScreenView.ts
@@ -1,4 +1,4 @@
-// Copyright 2024, University of Colorado Boulder
+// Copyright 2024-2025, University of Colorado Boulder
 
 /**
  * MobiusScreenView extends PhET's ScreenView to include functionality specific to 3D model-to-view and view-to-model
